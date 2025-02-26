96c32d84c23aeadba0991cd6c953c86e1c1b6939
rever <rever@ARTEM>
2025-02-26 16:37:25 +0300

    add potion drink

--- ConsoleGameKurzaev/Program.cs
+++ ConsoleGameKurzaev/Program.cs
@@ -14,14 +14,14 @@ namespace ConsoleGameKurzaev
             int health = 100;
             int gold = 50;
             int arrows = 20;
-            List<string> inventory = new List<string>() { "Мечь", "Стрелы" }; // Initial inventory
+            List<string> inventory = new List<string>() { "Мечь", "Стрелы" }; 
 
             
             string[] dungeonMap = GenerateDungeonMap();
 
             Console.WriteLine("Добро пожаловать в Number Quest PLUS!");
 
-            bool gameEnded = false; // Flag to determine if the game has ended
+            bool gameEnded = false; 
 
             
             for (int roomNumber = 0; roomNumber < dungeonMap.Length; roomNumber++)
@@ -41,7 +41,7 @@ namespace ConsoleGameKurzaev
                         (inventory, gold, arrows) = FindChest(inventory, gold, arrows);
                         break;
                     case "Trader":
-                        gold = MeetTrader(gold, health, inventory); // Pass inventory
+                        gold = MeetTrader(gold, health, inventory); 
                         break;
                     case "Empty Room":
                         Console.WriteLine("Комната пуста. Здесь ничего не происходит.");
@@ -56,7 +56,7 @@ namespace ConsoleGameKurzaev
                 {
                     Console.WriteLine("\nВы погибли в подземелье. Игра окончена.");
                     gameEnded = true;
-                    break; // Exit the loop
+                    break; 
                 }
 
                 if (roomNumber == dungeonMap.Length - 1 && currentRoom == "Boss")
@@ -64,7 +64,7 @@ namespace ConsoleGameKurzaev
                     Console.WriteLine("\nПоздравляем! Вы победили Босса и прошли Number Quest PLUS!");
                     Console.WriteLine($"Ваши итоговые показатели: Здоровье: {health}, Золото: {gold}, Стрелы: {arrows}, Инвентарь: {string.Join(", ", inventory)}");
                     gameEnded = true;
-                    break; // Exit the loop
+                    break; 
                 }
 
                 Console.WriteLine($"Состояние: Здоровье: {health}, Золото: {gold}, Стрелы: {arrows}, Инвентарь: {string.Join(", ", inventory)}");
@@ -84,7 +84,7 @@ namespace ConsoleGameKurzaev
             if (gameEnded)
             {
                 Console.WriteLine("\nНажмите любую клавишу для выхода...");
-                Console.ReadKey(); // Await keystroke
+                Console.ReadKey(); 
             }
         }
 
@@ -100,7 +100,7 @@ namespace ConsoleGameKurzaev
                 dungeonMap[i] = events[rnd.Next(events.Length)];
             }
 
-            dungeonMap[9] = "Boss"; // Boss is always at the end
+            dungeonMap[9] = "Boss"; 
             return dungeonMap;
         }
 
@@ -108,7 +108,7 @@ namespace ConsoleGameKurzaev
         static (int, int, int) BattleMonster(int health, int gold, int arrows)
         {
             Random rnd = new Random();
-            int monsterHealth = rnd.Next(25, 61); // Monster is stronger
+            int monsterHealth = rnd.Next(25, 61); 
 
             Console.WriteLine("Внезапно! Перед вами возник злобный монстр!");
 
@@ -129,11 +129,11 @@ namespace ConsoleGameKurzaev
 
                 switch (choice)
                 {
-                    case "1": // Sword
+                    case "1": 
                         playerDamage = rnd.Next(12, 23);
                         Console.WriteLine($"Вы рубите мечом и наносите {playerDamage} урона!");
                         break;
-                    case "2": // Bow
+                    case "2": 
                         if (arrows > 0)
                         {
                             playerDamage = rnd.Next(7, 18);
@@ -199,11 +199,11 @@ namespace ConsoleGameKurzaev
 
                 switch (choice)
                 {
-                    case "1": // Sword
+                    case "1": 
                         playerDamage = rnd.Next(18, 29);
                         Console.WriteLine($"Вы яростно рубите мечом, нанося {playerDamage} урона!");
                         break;
-                    case "2": // Bow
+                    case "2": 
                         if (arrows > 0)
                         {
                             playerDamage = rnd.Next(13, 24);
@@ -265,7 +265,7 @@ namespace ConsoleGameKurzaev
             Random rnd = new Random();
             int num1 = rnd.Next(2, 13);
             int num2 = rnd.Next(2, 13);
-            int operation = rnd.Next(1, 4); // 1 - сложение, 2 - вычитание, 3 - умножение
+            int operation = rnd.Next(1, 4); 
             int correctAnswer = 0;
             string operationSymbol = "";
 
@@ -328,7 +328,7 @@ namespace ConsoleGameKurzaev
         }
 
         
-        static int MeetTrader(int gold, int health, List<string> inventory) // inventory is passed
+        static int MeetTrader(int gold, int health, List<string> inventory) 
         {
             Console.WriteLine("Приветствую! Я - торговец. Что желаете?");
             Console.WriteLine($"У вас {gold} золота. Зелье исцеления - 30 золотых. Купите? (y/n)");
